fccb3ea1e71bb3b19b3e3e9445fb342a3d9282b8
Sammy Stollman <stollmans@wit.edu>
2023-04-07 15:30:10 -0400

    Added module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.cookieclicker {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.base;
+    requires java.desktop;
 
 
     opens com.example.cookieclicker to javafx.fxml;
